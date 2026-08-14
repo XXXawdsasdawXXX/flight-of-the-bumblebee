79bd121e8a0880a8d5afe68782c2eef7f88de117
XXXawdsasdawXXX <xxx.awdsasdaw.xxx@gmail.com>
2026-08-14 19:08:31 +0300

    fix movement

--- Scripts/BeeMovement.gd
+++ Scripts/BeeMovement.gd
@@ -1,6 +1,7 @@
 @tool
 extends Node2D
 
+@export var view: AnimatedSprite2D
 @export var base_speed: float = 120
 @export var speed_amp: float = 40
 @export var speed_freq: float = 1.5
@@ -18,48 +19,62 @@ var _noise := FastNoiseLite.new()
 var _time: float 
 var _way_progress: float # 0 - старт. 1 - конец
 var _is_going_forward: bool
+var _path_lenght: float
 
 
 func  _ready() -> void:
+	queue_redraw()
+	
+	if Engine.is_editor_hint():
+		return
+	
+	_path_lenght = start_pos.distance_to(end_pos)
 	position = start_pos
-	#boot noise
+	view.position = Vector2.ZERO
+	view.flip_h = start_pos.x > end_pos.x
+	
 	_noise.seed = randi()
 	_noise.frequency = 0.4
 	_noise.noise_type = FastNoiseLite.TYPE_SIMPLEX_SMOOTH
+	
+
 
 func _process(delta: float) -> void:
+	if Engine.is_editor_hint():
+		return
+	
 	_time += delta
 	_move(delta)
 
 
 func _move(delta: float) -> void:
-	var path_len := end_pos.length()
-	if path_len < 0.001:
+	if _path_lenght < 0.001:
 		return
 	
 	var current_speed := base_speed + sin(_time * speed_freq) * speed_amp
-	var step := (current_speed * delta) / path_len
+	var step := (current_speed * delta) / _path_lenght
 	
 	if _is_going_forward:
 		_way_progress += step
 		if _way_progress >= 1:
 			_way_progress = 1
 			_is_going_forward = false
+			view.flip_h = _is_going_forward
 	else:
 		_way_progress -= step
 		if _way_progress <= 0:
 			_way_progress = 0
 			_is_going_forward = true
+			view.flip_h = _is_going_forward
 		
 	var point := start_pos.lerp(end_pos, _way_progress)
-	position.x = point.x	
-	position.y = point.x + _noise.get_noise_1d(_time) * wave_amp	
+	position = point	
+	view.position = Vector2(0, _noise.get_noise_1d(_time) * wave_amp)
 
 
 func _draw() -> void:
-	if Engine.is_editor_hint():
-		draw_line(
-			start_pos, 
-			end_pos, 
-			Color.CADET_BLUE, 
-			2)
+	var from := to_local(start_pos)
+	var to := to_local(end_pos)
+	draw_line(from, to, Color.CADET_BLUE,2)
+	draw_circle(from, 4, Color.YELLOW)	
+	draw_circle(to, 4, Color.YELLOW)	
